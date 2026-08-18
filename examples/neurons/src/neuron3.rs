c275540b3b95d4f3624f79ea9fa22a5444b2c9cd
John Wooten <noreply@wooten.link>
2026-08-18 11:58:28 -0400

    🐛 Handle missing users in Neuron3 example

--- examples/neurons/src/neuron3.rs
+++ examples/neurons/src/neuron3.rs
@@ -27,7 +27,7 @@ impl Neuron for Neuron3 {
         let mut result = HashMap::new();
 
         for user in users {
-            let bonus: f64 = Neuron3::bonus(*self.data.get(user).unwrap());
+            let bonus: f64 = Neuron3::bonus(*self.data.get(user).unwrap_or(&0.0));
             result.insert(user.into(), bonus);
         }
 
